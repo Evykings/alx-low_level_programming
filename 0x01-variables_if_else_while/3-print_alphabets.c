eeeb862d84e0040235c9fc87e249e77901140878
evykings <evansakpowan@gmail.com>
2022-09-14 14:33:15 -0700

    my commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,22 +12,22 @@ int main(void)
 
 {
 
-		char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-			int i;
+	int i;
 
 
 
-				for (i = 0; i < 52; i++)
+	for (i = 0; i < 52; i++)
 
-						{
+	{
 
-									putchar(alp[i]);
+	putchar(alp[i]);
 
-										}
+	}
 
-					putchar('\n');
+	putchar('\n');
 
-						return (0);
+	return (0);
 
 }
