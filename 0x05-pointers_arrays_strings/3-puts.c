e8ba0dbdb379aea11217c92d8832ff91979b19b5
evykings <evansakpowan@gmail.com>
2022-09-20 15:31:34 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -12,23 +12,15 @@ void _puts(char *str)
 
 int count = 0;
 
-while (count >= 0)
+while (*(str + count) != '\0')
 
 {
 
-if (str[count] == '\0')
-
-{
-_putchar('\n');
-
-break;
-
-}
-
-_putchar(str[count]);
+putchar(*(str + count));
 
 count++;
 
 }
+putchar(10);
 
 }
